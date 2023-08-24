62912c515a86afe7c47c9ed5161797fcc91e6fd0
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-08-24 12:03:32 -0400

    Update touhou script

--- touhou.py
+++ touhou.py
@@ -1,27 +1,39 @@
 import pyautogui
 import keyboard
 
-print("Let's play Touhou! Press 'c' to hold fire")
+print("Let's play Touhou! Press 'c' to hold fire, 'a' to hold slow movement.")
 print("Press 'q' to exit")
 
-key_down = False
+
+def toggle_action(tap_key, hold_key, key_down):
+    if keyboard.is_pressed(tap_key):
+        if key_down:
+            pyautogui.keyUp(hold_key)
+            key_down = False
+        else:
+            pyautogui.keyDown(hold_key)
+            key_down = True
+        while keyboard.is_pressed(tap_key):
+            pass
+
+    if keyboard.is_pressed(hold_key) and key_down:
+        pyautogui.keyUp(hold_key)
+        key_down = False
+
+    return key_down
+
+
+fire_key_down = False
+slow_key_down = False
 
 while True:
     # Exit program
     if keyboard.is_pressed('q'):
         break
+    # Auto-fire
+    fire_key_down = toggle_action('c', 'z', fire_key_down)
 
-    # Start auto-fire
-    if keyboard.is_pressed('c'):
-        while keyboard.is_pressed('c'):
-            pass
-        # Release
-        if key_down:
-            pyautogui.keyUp('z')
-            key_down = False
-        # Activate
-        else:
-            pyautogui.keyDown('z')
-            key_down = True
+    # Slow movement
+    slow_key_down = toggle_action('a', 'shift', slow_key_down)
 
 print("Touhou time over, GG")
