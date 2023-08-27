a22de7ea16a1a04ba65cdd09f0c3e16aade27013
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-08-27 11:31:12 -0400

    Remove slow down hold. Not needed

--- touhou.py
+++ touhou.py
@@ -1,22 +1,22 @@
 import pyautogui
 import keyboard
 
-print("Let's play Touhou! Press 'c' to hold fire, 'a' to hold slow movement.")
+print("Let's play Touhou! Press 'c' to hold fire")
 print("Press 'q' to exit")
 
 
 def toggle_action(tap_key, hold_key, key_down):
     if keyboard.is_pressed(tap_key):
+        print("Holding " + hold_key)
         if key_down:
             pyautogui.keyUp(hold_key)
             key_down = False
         else:
             pyautogui.keyDown(hold_key)
             key_down = True
-        while keyboard.is_pressed(tap_key):
-            pass
 
     if keyboard.is_pressed(hold_key) and key_down:
+        print("Quit holding " + hold_key)
         pyautogui.keyUp(hold_key)
         key_down = False
 
@@ -30,10 +30,8 @@ while True:
     # Exit program
     if keyboard.is_pressed('q'):
         break
+
     # Auto-fire
     fire_key_down = toggle_action('c', 'z', fire_key_down)
-
-    # Slow movement
-    slow_key_down = toggle_action('a', 'shift', slow_key_down)
 
 print("Touhou time over, GG")
